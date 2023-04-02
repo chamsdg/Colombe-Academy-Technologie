c26f86c2ed2bc2efe765654177cf1f908fdd3f52
Chamsedine AIDARA <50946856+chamsdg@users.noreply.github.com>
2023-04-02 13:55:18 +0000

    Update colomb_academy_technologie.py

--- colomb_academy_technologie.py
+++ colomb_academy_technologie.py
@@ -18,220 +18,97 @@ st.set_page_config(layout="wide")
 st.image("cat.jfif", width=200)
 
 # afficher le titre.
-st.title("Master Data Science & IA dashboard")
+st.title("Licence Data Science & IA dashboard")
     
 # trois colonnes pour la progression et les modules
 progression, niveau, module, professeur = st.columns(4)
 #---------------------------------------------------------------------------------------------#
-#                                    MASTER 1                                                 #
+#                                    Licence 3                                                 #
 #---------------------------------------------------------------------------------------------#
 # le niveau de la filiere.
 with niveau:
     select_niveau = st.selectbox(
         "Merci de selectionner le niveau d'étude",
-        ("Master 1 Data & IA","Master 2 Data & IA", "Licence 3 Data"),)
-    if select_niveau == "Master 1 Data & IA":
+        ("Master 1 Data & IA","Master 2 Data & IA", "Licence Data & IA"),)
+    if select_niveau == "Licence Data & IA":
         with module:
             select_module = st.selectbox(
                 "Merci de selectionner le module",
-                ("Machine Learning","Deep Learning", "Data Science"),)
-            if  select_module == "Machine Learning":
+                ("Introduction Python","Statistique Descriptive", "Algorithme"),)
+            if  select_module == "Introduction Python":
                 with progression:
                     progression_percentage = st.slider(
                         "Estimation de la progression du cours.",
                         min_value = 0.0,
                         max_value = 100.0,
-                        value = 45.0,
+                        value = 90.0,
                         disabled = True,
                         format = "%d%%")
                 with st.expander("Compte rendu du module selectionné"):
                   st.markdown('''
-                        - apprentissage supervisé et non supervisé.
-                        - Explication detaillée sur l'overfitting effectuée'
+                        - objectifs du modules identifiés.
+                        - installation de pycharme
+                        - base de python(variales, boucles, fonctions,condtions etc)
+                        - programme arithmetique (soustraction, addition, multiplication et division)
+                        - fonction avancéé
+                        - programmation orienté object
+                        - creer un questionnaire
+                        - programme qui evalue la note d'un utlisateur
+                        - programme qui permet de trouver le conducteur le plus proche
+                        - Python et les bases de données
+                        - connexion base de données depuis python (creation de base de données, de tables,insertion des données et requetes etc)
+                        -
                         ''')
                 with professeur:
                     nom_professeur = st.selectbox(
                         "Le professeur chargé du cours",
                         ("Chamsedine AIDARA",""),disabled = True,)
                         
-            if  select_module == "Deep Learning":
+            if  select_module == "Statistique Descriptive":
                 with progression:
                     progression_percentage = st.slider(
                         "Estimation de la progression du cours.",
                         min_value = 0.0,
                         max_value = 100.0,
-                        value = 65.0,
+                        value = 60.0,
                         disabled = True,
                         format = "%d%%")
                 with st.expander("Compte rendu du module selectionné"):
                   st.markdown('''
-                        - Les applications du deep learning.
-                        - Classification avec les reseaux de neurones artificielles.
+                        - Manipulation des données.
+                        - Representation graphique des données
+                        - Les caracteristiques de position et de dispersion
+                        - distribution statistiques à deux caracteres
                         ''')  
                 with professeur:
                     nom_professeur = st.selectbox(
                         "Le professeur chargé du cours",
-                        ("Djibril Diop", " "),disabled = True,)
+                        ("Wahab DIOP", " "),disabled = True,)
                     
-            if  select_module == "Data Science":
+            if  select_module == "Algorithme":
                 with progression:
                     progression_percentage = st.slider(
                         "Estimation de la progression du cours.",
                         min_value = 0.0,
                         max_value = 100.0,
-                        value = 25.0,
+                        value = 50.0,
                         disabled = True,
                         format = "%d%%")
                 with st.expander("Compte rendu du module selectionné"):
                   st.markdown('''
-                        - Bon debut dans l' ensemble.
+                        - Prise de contact
+                        - Structure des données
+                        - Les Pointeurs
+                        - Les Modules
+                        - Liste Mono directionnelle
                         - Decouvert des librairies.
                         ''') 
                 with professeur:
                     nom_professeur = st.selectbox(
                         "Le professeur chargé du cours",
-                        ("Moussa Diop", " "),disabled = True,)
+                        ("To ask at Malick", " "),disabled = True,)
   
     
-#---------------------------------------------------------------------------------------------#
-#                                    MASTER 2                                                 #
-#---------------------------------------------------------------------------------------------#
-
-    with niveau:
-        if select_niveau == "Master 2 Data & IA":
-            with module:
-                select_module = st.selectbox(
-                    "Merci de selectionner le module",
-                    ("DataWarehouse","Computer Vision", "ElasticSearch"),)
-                if  select_module == "DataWarehouse":
-                    with progression:
-                        progression_percentage = st.slider(
-                            "Estimation de la progression du cours.",
-                            min_value = 0.0,
-                            max_value = 100.0,
-                            value = 50.0,
-                            disabled = True,
-                            format = "%d%%")
-                    with st.expander("Compte rendu du module selectionné"):
-                      st.markdown('''
-                            - Donner plus d'exercice.
-                            - Plus d'explication sur les concepts.
-                            ''')   
-                    with professeur:
-                        nom_professeur = st.selectbox(
-                            "Le professeur chargé du cours",
-                            ("Moustapha Mané",""),disabled = True,)
-                            
-                if  select_module == "Computer Vision":
-                    with progression:
-                        progression_percentage = st.slider(
-                            "Estimation de la progression du cours.",
-                            min_value = 0.0,
-                            max_value = 100.0,
-                            value = 85.0,
-                            disabled = True,
-                            format = "%d%%")
-                    with st.expander("Compte rendu du module selectionné"):
-                      st.markdown('''
-                            - Travailler davantage sur les images.
-                            - Reconnaissance de video.
-                            ''')  
-                    with professeur:
-                        nom_professeur = st.selectbox(
-                            "Le professeur chargé du cours",
-                            ("Malick",""),disabled = True,)
-                        
-                if  select_module == "ElasticSearch":
-                    with progression:
-                        progression_percentage = st.slider(
-                            "Estimation de la progression du cours.",
-                            min_value = 0.0,
-                            max_value = 100.0,
-                            value = 95.0,
-                            disabled = True,
-                            format = "%d%%")
-                    with st.expander("Compte rendu du module selectionné"):
-                      st.markdown('''
-                            - L'utilité de ElasticSearch.
-                            - Dashboard en temps réel.
-                            ''') 
-                    with professeur:
-                        nom_professeur = st.selectbox(
-                            "Le professeur chargé du cours",
-                            ("Moustapha Touré",""),disabled = True,)
-
-#---------------------------------------------------------------------------------------------#
-#                                    LICENCE 3                                                #
-#---------------------------------------------------------------------------------------------#
-      
-    with niveau:
-        if select_niveau == "Licence 3 Data":
-            with module:
-                select_module = st.selectbox(
-                    "Merci de selectionner le module",
-                    ("Culture et Ethique IA","Web Scraping", "Data Visualisation"),)
-                if  select_module == "Culture et Ethique IA":
-                    with progression:
-                        progression_percentage = st.slider(
-                            "Estimation de la progression du cours.",
-                            min_value = 0.0,
-                            max_value = 100.0,
-                            value = 100.0,
-                            disabled=True,
-                            format = "%d%%")
-                    with st.expander("Compte rendu du module selectionné"):
-                      st.markdown('''
-                            - La problématique de L'IA.
-                            - Les limites de L'IA.
-                            ''')   
-                    with professeur:
-                        nom_professeur = st.selectbox(
-                            "Le professeur chargé du cours",
-                            ("Malainy Aidara",""),disabled = True,)
-                            
-                if  select_module == "Web Scraping":
-                    with progression:
-                        progression_percentage = st.slider(
-                            "Estimation de la progression du cours.",
-                            min_value = 0.0,
-                            max_value = 100.0,
-                            value = 95.0,
-                            disabled = True,
-                            format = "%d%%")
-                    with st.expander("Compte rendu du module selectionné"):
-                      st.markdown('''
-                            - Lister les librairies de python.
-                            - load depuis wikipedia toutes les regions du senegal.
-                            ''')  
-                            
-                    with professeur:
-                        nom_professeur = st.selectbox(
-                            "Le professeur chargé du cours",
-                            ("Abdoulaye Sow"," "),disabled = True,)
-                    
-                    
-                if  select_module == "Data Visualisation":
-                    with progression:
-                        progression_percentage = st.slider(
-                            "Estimation de la progression du cours.",
-                            min_value = 0.0,
-                            max_value = 100.0,
-                            value = 15.0,
-                            disabled = True,
-                            format = "%d%%")
-                    with st.expander("Compte rendu du module selectionné"):
-                      st.markdown('''
-                            - Comprehension des concepts de power Bi.
-                            - Ces differentes applications.
-                            ''') 
-                        
-                    with professeur:
-                        nom_professeur = st.selectbox(
-                            "Le professeur chargé du cours",
-                            ("Pape Moussa Gueye"," "),disabled = True,)
-
-
-
 #---------------------------------------------------------------------------------------------#
 #                                   DIVERS                                                    #
 #---------------------------------------------------------------------------------------------#
